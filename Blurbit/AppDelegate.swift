2b95baad37eda82eb4b0991618f6c86047716a85
JuliaHeleneW <gyros.chips@gmail.com>
2020-04-12 22:40:48 -0700

    Login/Signup features

--- Blurbit/AppDelegate.swift
+++ Blurbit/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "blurbit"
+                configuration.clientKey="voSrYYISRs4Ex18QPtqL"
+                configuration.server = "https://blurbit.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
