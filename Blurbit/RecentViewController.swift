ed5d6c4c3acaa87372c3687b37147fc46c3dc106
admin <admin@admins-Mac.local>
2020-04-14 23:17:45 -0700

    create new search record, load search records

--- Blurbit/RecentViewController.swift
+++ Blurbit/RecentViewController.swift
@@ -2,7 +2,7 @@
 //  RecentViewController.swift
 //  Blurbit
 //
-//  Created by administrator on 4/12/20.
+//  Created by Frank Piva on 4/12/20.
 //  Copyright © 2020 Team-Blurbit. All rights reserved.
 //
 
@@ -12,41 +12,45 @@ import UIKit
 class RecentViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var recentTableView: UITableView!
-    var recents = [PFObject]()
+    var searches = [PFObject]()
 
     override func viewDidLoad() {
         print("RecentViewController.swift: viewDidLoad()")
         super.viewDidLoad()
         self.recentTableView.dataSource = self
         self.recentTableView.delegate = self
-        let query = PFQuery(className: "Book")
-        query.includeKeys(["author", "title"])
-        query.limit = 20
-        query.findObjectsInBackground { (books, error) in
-            if (books != nil) {
-                self.recents = books!
-                self.recentTableView.reloadData()
-            } else {
-                print("FeedViewController.swift: \(error?.localizedDescription)")
-            }
-        }
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("RecentViewController.swift: tableView()")
-        return recents.count // TODO: make this dynamic
+        self.loadSearches()
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("RecentViewController.swift: tableView()")
+        print("RecentViewController.swift: tableView(cellForRowAt)")
         let cell = tableView.dequeueReusableCell(withIdentifier: "RecentTableViewCell") as! RecentTableViewCell
-        
-        let recent = recents[indexPath.row]
-        cell.bookAuthor.text = recent["author"] as? String
-        print(cell.bookAuthor.text)
-        cell.bookTitle.text = recent["title"] as? String
-        print(cell.bookTitle.text)
+        let search = searches[indexPath.row]
+        cell.bookAuthor.text = search["author"] as? String
+        cell.bookTitle.text = search["title"] as? String
         return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("RecentViewController.swift: tableView(numberOfRowsInSection)")
+        return searches.count
+    }
+
+    func loadSearches() {
+        print("RecentViewController.swift: loadSearches()")
+        let query = PFQuery(className: "Search")
+        query.limit = 10
+        query.includeKeys(["author", "title"])
+        query.whereKey("user", equalTo: PFUser.current()!)
+        query.findObjectsInBackground { (searches, error) in
+            if (searches != nil) {
+                self.searches = searches!
+                self.recentTableView.reloadData()
+            } else {
+                let message = error?.localizedDescription ?? "error loading search records"
+                print("FeedViewController.swift: \(message)")
+            }
+        }
     }
 
 }
--- Blurbit/ReviewsViewController.swift
+++ Blurbit/ReviewsViewController.swift
@@ -6,12 +6,12 @@
 //  Copyright © 2020 Team-Blurbit. All rights reserved.
 //
 
-import UIKit
 import AlamofireImage
+import Parse
+import UIKit
 
 class ReviewsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    
-    
+
     var gtin=""
     var imageURL=URL(string:"https://camo.githubusercontent.com/e69c2fe22d071aa6c93355e008fe56a9aff0a14a/68747470733a2f2f692e696d6775722e636f6d2f763257514346412e706e67")!
     var bookTitle = "unknown title"
@@ -19,25 +19,25 @@ class ReviewsViewController: UIViewController,UITableViewDelegate,UITableViewDat
     var ratingNum = 0
     var ratingsTotal = 0
     var reviews=[[String:Any]]()
-    
+
     @IBOutlet weak var tableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
         startSetup();
     }
-    
+
     func startSetup(){
+        print("ReviewsViewController.swift: startSetup()")
         print(self.gtin)
         tableView.delegate=self
         tableView.dataSource=self
         loadReviews()
-        
     }
-    
+
     func loadReviews(){
+        print("ReviewsViewController.swift: loadReviews()")
         //test GTIN:9780141362250
         let url = URL(string: "https://api.rainforestapi.com/request?api_key=379913B5856E4E079E13E66CDD814EB9&type=reviews&amazon_domain=amazon.com&gtin="+self.gtin)!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -45,19 +45,19 @@ class ReviewsViewController: UIViewController,UITableViewDelegate,UITableViewDat
         let task = session.dataTask(with: request) { (data, response, error) in
             // This will run when the network request returns
             if let error = error {
-                print(error.localizedDescription)
+                print("ReviewsViewController.swift: ", error.localizedDescription)
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            
-            self.reviews=dataDictionary["reviews"] as! [[String:Any]]
+                // TODO: Unexpectedly found nil while unwrapping an Optional value
+                self.reviews=dataDictionary["reviews"] as! [[String:Any]]
                 let product=dataDictionary["product"] as! [String:Any]
                 if let imageData=product["image"] as? String{
                     let imageUrl=URL(string:imageData)!
-                self.imageURL=imageUrl
-            }
-            if let title=product["title"] as? String{
-                self.bookTitle=title
-            }
+                    self.imageURL=imageUrl
+                }
+                if let title=product["title"] as? String{
+                    self.bookTitle=title
+                }
                 let author=product["sub_title"] as! [String:Any]
                 if let bookAuthor=author["text"] as? String{
                 self.authorName=bookAuthor
@@ -67,7 +67,7 @@ class ReviewsViewController: UIViewController,UITableViewDelegate,UITableViewDat
                         let firstName=splitChars[1]
                         self.authorName=firstName+" "+lastName as String
                     }
-            }
+                }
                 let ratings=dataDictionary["summary"] as! [String:Any]
                 if let ratingNumber=ratings["rating"] as? Double{
                     self.ratingNum=Int(ratingNumber)
@@ -75,53 +75,64 @@ class ReviewsViewController: UIViewController,UITableViewDelegate,UITableViewDat
                 if let ratingtotal=ratings["ratings_total"] as? Int{
                     self.ratingsTotal=ratingtotal
                 }
-                print(self.reviews[0])
-                //print(product)
-            self.tableView.reloadData()
+                // print(self.reviews[0])
+                // print(product)
+                self.tableView.reloadData()
             }
         }
         task.resume()
     }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("ReviewsViewController.swift: tableView(numberOfRowsInSection)")
         return reviews.count
-   }
-   
-   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-   if indexPath.row == 0 {
-       let cell=tableView.dequeueReusableCell(withIdentifier: "BookCell") as! BookViewCell
-       cell.bookTitle.text=self.bookTitle
-       cell.bookAuthor.text="By "+self.authorName
-    print(self.ratingNum)
-    let ratingImageName="stars_\(self.ratingNum).png" as String
-    cell.ratingView.image=UIImage(named:ratingImageName)
-    let ratingText="\(self.ratingsTotal) customer ratings"
-    cell.ratingLabel.text=ratingText
-    cell.bookImage.af_setImage(withURL: self.imageURL)
-       return cell
-   }
-   else {
-       let cell=tableView.dequeueReusableCell(withIdentifier: "ReviewCell") as! ReviewViewCell
-       let review=reviews[indexPath.row]
-    print(review)
-    let ratingNumb=review["rating"] as! Int
-    cell.titleLabel.text=review["title"] as? String
-    let imageName="stars_\(ratingNumb).png" as String
-    cell.ratingView.image=UIImage(named:imageName)!
-    let profile=review["profile"] as! [String:Any]
-       cell.usernameLabel.text=profile["name"] as? String
-       cell.reviewLabel.text=review["body"] as? String
-       return cell
-   }
-}
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("ReviewsViewController.swift: tableView(cellForRowAt)")
+        if indexPath.row == 0 {
+            let cell=tableView.dequeueReusableCell(withIdentifier: "BookCell") as! BookViewCell
+            cell.bookTitle.text=self.bookTitle
+            cell.bookAuthor.text="By "+self.authorName
+            // create new Searche record
+            self.createSearch()
+            // print(self.ratingNum)
+            let ratingImageName="stars_\(self.ratingNum).png" as String
+            cell.ratingView.image=UIImage(named:ratingImageName)
+            let ratingText="\(self.ratingsTotal) customer ratings"
+            cell.ratingLabel.text=ratingText
+            cell.bookImage.af_setImage(withURL: self.imageURL)
+            return cell
+        }
+        else {
+            let cell=tableView.dequeueReusableCell(withIdentifier: "ReviewCell") as! ReviewViewCell
+            let review=reviews[indexPath.row]
+            // print(review)
+            let ratingNumb=review["rating"] as! Int
+            cell.titleLabel.text=review["title"] as? String
+            let imageName="stars_\(ratingNumb).png" as String
+            cell.ratingView.image=UIImage(named:imageName)!
+            let profile=review["profile"] as! [String:Any]
+            cell.usernameLabel.text=profile["name"] as? String
+            cell.reviewLabel.text=review["body"] as? String
+            return cell
+        }
+    }
+
+    func createSearch() {
+        print("ReviewsViewController.swift: createSearch()")
+        let search = PFObject(className: "Search")
+        search["author"] = self.authorName
+        search["title"] = self.bookTitle
+        search["user"] = PFUser.current()!
+        search.saveInBackground { (success, error) in
+            if (success) {
+                print("ReviewsViewController.swift: search record saved")
+           } else {
+                let message = error?.localizedDescription ?? "error creating search record"
+                print("ReviewsViewController.swift: \(message)")
+           }
+        }
+    }
 
 }
