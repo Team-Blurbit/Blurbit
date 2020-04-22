ae7cfeaec8da090a749db8589b3bbda21648f343
admin <admin@admins-Mac.local>
2020-04-22 13:39:46 -0700

    add open_now and weekday_text to MapsDetailsViewController

--- Blurbit/MapDetailsViewController.swift
+++ Blurbit/MapDetailsViewController.swift
@@ -12,12 +12,16 @@ class MapDetailsViewController: UIViewController {
 
     var detailsTask: URLSessionDataTask!
     var key: String = "AIzaSyAiPLeK9PFJzvGlAugRivosfuBjsk9ixSE"
+    var name: String = ""
+    var openNow: Int = 0
+    var weekdayTexts = [String]()
     var placeId: String = ""
 
     override func viewDidLoad() {
         print("MapDetailsViewController.swift: viewDidLoad()")
         super.viewDidLoad()
         let urlString = "https://maps.googleapis.com/maps/api/place/details/json?key=\(key)&place_id=\(placeId)"
+        print(urlString)
         let urlVar = URL(string: urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)!
         var request=URLRequest(url: urlVar, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 50)
         request.httpMethod = "GET"
@@ -25,12 +29,27 @@ class MapDetailsViewController: UIViewController {
             if let error = error {
                 print("MapDetailsViewController.swift: \(error.localizedDescription)")
             }
+            // unpack data
             if let data = data {
                 if let json = try! JSONSerialization.jsonObject(with: data,options:[]) as? [String:Any]{
                     if let result = json["result"] as? [String:Any] {
                         if let name = result["name"] as? NSString {
-                            let storeName = name as String
-                            print(storeName)
+                            self.name = name as String
+                            print(self.name)
+                        }
+                        if let openingHours = result["opening_hours"] as? [String: Any] {
+                            if let openNow = openingHours["open_now"] as? Int {
+                                self.openNow = openNow
+                                print(self.openNow)
+                            }
+                            if let weekdayTexts = openingHours["weekday_text"] as? NSArray {
+                                for index in 0 ..< weekdayTexts.count {
+                                    if let weekdayText = weekdayTexts[index] as? String {
+                                        self.weekdayTexts.append(weekdayText)
+                                    }
+                                }
+                                print(self.weekdayTexts)
+                            }
                         }
                     }
                 }
