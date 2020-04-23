0ecc3c6bf9523895a17a96913336ed9819517cf9
Justin Betts <jbetts@csumb.edu>
2020-04-22 20:51:30 -0700

    search by title view controller exists

---
+++ Blurbit/BookCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookCell.swift
+//  Blurbit
+//
+//  Created by Jesse Betts on 4/13/20.
+//  Copyright © 2020 Team-Blurbit. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
