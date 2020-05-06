891146ba06418c6b3bcf5d55a953ab2b834fcc1a
Frank Piva <piva.post@gmail.com>
2020-05-06 14:03:41 -0700

    add new model to FeaturedViewController

--- Blurbit/FeaturedViewController.swift
+++ Blurbit/FeaturedViewController.swift
@@ -26,13 +26,15 @@ class FeaturedViewController: UIViewController, UITableViewDataSource, UITableVi
         "Holes": "Louis Sachar"
     ]
 
-    var recommendations: [NSDictionary] = []
-
+    var recommendations: [[String: Any]] = []
+    var reviews: [String: Double] = [:]
+    
     override func viewDidLoad() {
         print("FeaturedViewController.swift: viewDidLoad()")
         super.viewDidLoad()
         self.featuredTableView.dataSource = self
         self.featuredTableView.delegate = self
+        self.loadReviews()
         self.loadPredictions()
     }
 
@@ -45,8 +47,10 @@ class FeaturedViewController: UIViewController, UITableViewDataSource, UITableVi
         print("FeaturedViewController.swift: tableView(cellForRowAt)")
         let cell = tableView.dequeueReusableCell(withIdentifier: "FeaturedTableViewCell") as! FeaturedTableViewCell
         let recommendation = self.recommendations[indexPath.row]
-        print(recommendation)
+        print("recommendation=<\(recommendation)>")
         cell.author.text = recommendation["author"] as? String
+        let url = URL(string: recommendation["imageUrl"]! as! String)
+        cell.coverView.af_setImage(withURL: url!)
         cell.rating.text = "5"
         cell.title.text = recommendation["title"] as? String
         return cell
@@ -56,26 +60,79 @@ class FeaturedViewController: UIViewController, UITableViewDataSource, UITableVi
         print("FeaturedViewController.swift: tableView(numberOfRowsInSection)")
         return self.recommendations.count
     }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150.0
-    }
 
     func loadPredictions() {
-        print("FeatureViewController.swift: loadPredictions()")
+        print("FeaturedViewController.swift: loadPredictions()")
         let model = ItemSimilarityRecommender()
-        // items = list of ratings to evaluate the model
-        // k = number of recommendations
-        // retrict_ = ???
-        // exclude = ???
-        guard let prediction = try? model.prediction(input: ItemSimilarityRecommenderInput(items: ["Alice in Wonderland" : 3.0], k: 5, restrict_: ["None"], exclude: ["None"])) else {
+        // items = The list of items used to generate the recommendations.
+        // k = The number of items to return on a recommendation.
+        // retrict_ = A sequence of items from which to generate recommendations.
+        // exclude = A sequence of items to exclude from recommendations.  Defaults to the input item list if not given.
+        // if user has no reviews add a dummy review to seed the recommender
+        if self.reviews.count < 1 {
+            print("user has no reviews")
+            //self.reviews["foo"] = 5.0
+        }
+        guard let prediction = try? model.prediction(input: ItemSimilarityRecommenderInput(items: self.reviews, k: 5, restrict_: ["None"], exclude: ["None"])) else {
             fatalError("FeaturedViewController.swift: runtime error while generating prediction")
         }
-        print(prediction.recommendations)
+        print("prediction.recommendations=<\(prediction.recommendations)>")
         for recommendation in prediction.recommendations {
-            let author = database[recommendation] as! String
-            let title = recommendation
-            self.recommendations.append(["author": author, "title": title])
+            self.getBookById(bookId: recommendation)
+        }
+        self.featuredTableView.reloadData()
+    }
+
+    //func getBookById(bookId: String) -> [String: Any] {
+    func getBookById(bookId: String) {
+        print("FeaturedViewController.swift: getBookById(bookId: \(bookId))")
+        let query = PFQuery(className: "Book")
+        var detailedBook: [String: Any] = [:]
+        query.includeKeys(["author", "title"])
+        query.limit = 1
+        query.whereKey("objectId", equalTo: bookId)
+        query.findObjectsInBackground { (books, error) in
+            if books != nil {
+                print("found books")
+                print("books=<\(books!)>")
+                for book in books! {
+                    print("book[\"author\"]=<\(book["author"]!)>")
+                    print("book[\"imageUrl\"]!=<\(book["imageUrl"]!)>")
+                    print("book[\"isbn\"]=<\(book["isbn"]!)>")
+                    print("book[\"title\"]=<\(book["title"]!)>")
+                    detailedBook["author"] = book["author"]!
+                    detailedBook["imageUrl"] = book["imageUrl"]!
+                    detailedBook["isbn"] = book["isbn"]!
+                    detailedBook["title"] = book["title"]!
+                    print("detailedBook=<\(detailedBook)>")
+                    self.recommendations.append(detailedBook)
+                    self.featuredTableView.reloadData()
+                }
+            } else {
+                let message = error?.localizedDescription ?? "error loading book record"
+                print("FeaturedViewController.swift: \(message)")
+            }
+        }
+        print("detailedBook=<\(detailedBook)>")
+        //return detailedBook
+    }
+
+    func loadReviews() {
+        print("FeaturedViewController.swift: loadReviews()")
+        let query = PFQuery(className: "Review")
+        query.includeKeys(["bookId", "rating"])
+        query.limit = 10
+        query.whereKey("userId", equalTo: PFUser.current()!)
+        query.findObjectsInBackground { (reviews, error) in
+            if reviews != nil {
+                for review in reviews! {
+                    self.reviews["\(review["bookId"]!)"] = review["rating"] as? Double
+                    print("self.reviews=<\(self.reviews)>")
+                }
+            } else {
+                let message = error?.localizedDescription ?? "error loading review records"
+                print("FeaturedViewController.swift: \(message)")
+            }
         }
     }
 
